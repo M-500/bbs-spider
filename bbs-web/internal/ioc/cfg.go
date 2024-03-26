e53bf6fae1af823ad2cf727aff08118a3d9d0bbf
wulinlin <18574945291@163.com>
2024-03-26 15:10:51 +0800

    妈个鸡+1

--- bbs-web/internal/ioc/cfg.go
+++ bbs-web/internal/ioc/cfg.go
@@ -1,5 +1,38 @@
 package ioc
 
-// @Description
-// @Author 代码小学生王木木
-// @Date 2024-03-26 11:49
+type Config struct {
+	ServiceName    string         `mapstructure:"service_name"`
+	ServiceVersion string         `mapstructure:"service_version"`
+	ServicePort    string         `mapstructure:"service_port"`
+	PrometheusPort string         `mapstructure:"prometheus_port"`
+	Database       DatabaseConfig `mapstructure:"database"`
+	Redis          RedisConfig    `mapstructure:"redis"`
+}
+
+type ServerConfig struct {
+	Port string
+	Mode string
+}
+
+type DatabaseConfig struct {
+	DSN          string `mapstructure:"dsn"`
+	MaxIdleConns int    `mapstructure:"max_idle_conn"`
+	MaxOpenConns int    `mapstructure:"max_open_conn"`
+}
+
+type RedisConfig struct {
+	DSN string `mapstructure:"dsn"`
+}
+
+//func InitConfig(path string) {
+//	viper.SetConfigFile(path)
+//	err := viper.ReadInConfig()
+//	if err != nil {
+//		log.Fatalf("Failed to read config file: %v", err)
+//	}
+//
+//	err = viper.Unmarshal(&AppConfig)
+//	if err != nil {
+//		log.Fatalf("Failed to unmarshal config file: %v", err)
+//	}
+//}
--- bbs-web/internal/ioc/db.go
+++ bbs-web/internal/ioc/db.go
@@ -3,3 +3,23 @@ package ioc
 // @Description
 // @Author 代码小学生王木木
 // @Date 2024-03-26 11:44
+
+import (
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+func InitDatabase(cfg *Config) *gorm.DB {
+	config := &gorm.Config{}
+	db, err := gorm.Open(mysql.Open(cfg.Database.DSN), config)
+	if err != nil {
+		panic(err)
+	}
+	if sqlDB, err := db.DB(); err == nil {
+		sqlDB.SetMaxIdleConns(cfg.Database.MaxIdleConns)
+		sqlDB.SetMaxOpenConns(cfg.Database.MaxOpenConns)
+	}
+	// TODO Prometheus监控
+
+	return db
+}
