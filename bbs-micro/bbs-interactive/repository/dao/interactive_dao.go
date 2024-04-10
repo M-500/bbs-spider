f328e611f876388977e2b36553bfd8a1f42f519d
wulinlin <18574945291@163.com>
2024-04-10 13:54:24 +0800

    dao接口定义

--- bbs-micro/bbs-interactive/repository/dao/interactive_dao.go
+++ bbs-micro/bbs-interactive/repository/dao/interactive_dao.go
@@ -13,8 +13,59 @@ type InteractiveDao interface {
 	DelLikeInfo(ctx context.Context, biz string, bizId int64, uid int64) error
 	IncrLikeCnt(ctx context.Context, biz string, id int64, uid int64) error
 	DecrLikeCnt(ctx context.Context, biz string, id int64, uid int64) error
-
 	Get(ctx context.Context, biz string, id int64) (InteractiveModel, error)
 	GetLikeInfo(ctx context.Context, biz string, id int64, uid int64) (UserLikeBizModel, error)
 	GetCollectInfo(ctx context.Context, biz string, id int64, uid int64) (UserCollectBizModel, error)
+}
+
+type interactiveDao struct {
+}
+
+func NewInteractiveDao() InteractiveDao {
+	return &interactiveDao{}
+}
+
+func (dao *interactiveDao) IncrReadCnt(ctx context.Context, biz string, bizId int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dao *interactiveDao) BatchIncrReadCnt(ctx context.Context, bizs []string, bizIds []int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dao *interactiveDao) IncrLikeInfo(ctx context.Context, biz string, id int64, uid int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dao *interactiveDao) DelLikeInfo(ctx context.Context, biz string, bizId int64, uid int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dao *interactiveDao) IncrLikeCnt(ctx context.Context, biz string, id int64, uid int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dao *interactiveDao) DecrLikeCnt(ctx context.Context, biz string, id int64, uid int64) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dao *interactiveDao) Get(ctx context.Context, biz string, id int64) (InteractiveModel, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dao *interactiveDao) GetLikeInfo(ctx context.Context, biz string, id int64, uid int64) (UserLikeBizModel, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (dao *interactiveDao) GetCollectInfo(ctx context.Context, biz string, id int64, uid int64) (UserCollectBizModel, error) {
+	//TODO implement me
+	panic("implement me")
 }
