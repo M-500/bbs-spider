4111798f8b47f53e7e672be92ad595caa55c831e
wulinlin <18574945291@163.com>
2024-04-10 14:27:59 +0800

    有毛病

--- bbs-micro/bbs-interactive/grpc/service.go
+++ bbs-micro/bbs-interactive/grpc/service.go
@@ -1,7 +1,9 @@
 package grpc
 
 import (
-	intrv1 "bbs-micro/api/bbs-micro/api/proto/gen/proto/intr/v1"
+	intrv1 "bbs-micro/api/proto/gen/proto/intr/v1"
+	"bbs-micro/bbs-interactive/domain"
+	"bbs-micro/bbs-interactive/service"
 	"context"
 )
 
@@ -12,16 +14,24 @@ import (
 // InteractiveServiceServer
 // @Description: 这里只是把service包装成一个grpc而已，和grpc有关的操作就在这里限定
 type InteractiveServiceServer struct {
+	svc service.InteractiveService
+	intrv1.UnimplementedInteractiveServiceServer
 }
 
 func (i *InteractiveServiceServer) IncrReadCnt(ctx context.Context, request *intrv1.IncrReadCntRequest) (*intrv1.IncrReadCntResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	err := i.svc.IncrReadCnt(ctx, request.Biz, request.BizId)
+	if err != nil {
+		return nil, err
+	}
+	return &intrv1.IncrReadCntResponse{}, nil // 只要error为nil 就不要返回数据域为nil 这个是标准写法
 }
 
 func (i *InteractiveServiceServer) Like(ctx context.Context, request *intrv1.LikeRequest) (*intrv1.LikeResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	err := i.svc.Like(ctx, request.GetBiz(), request.GetBizId(), request.GetUid()) // 使用Getxx()方法 要比.xx更优雅
+	if err != nil {
+		return nil, err
+	}
+	return &intrv1.LikeResponse{}, nil // 只要error为nil 就不要返回数据域为nil
 }
 
 func (i *InteractiveServiceServer) CancelLike(ctx context.Context, request *intrv1.CancelLikeRequest) (*intrv1.CancelLikeResponse, error) {
@@ -35,11 +45,27 @@ func (i *InteractiveServiceServer) CollectArt(ctx context.Context, request *intr
 }
 
 func (i *InteractiveServiceServer) Get(ctx context.Context, request *intrv1.GetRequest) (*intrv1.GetResponse, error) {
-	//TODO implement me
-	panic("implement me")
+	get, err := i.svc.Get(ctx, request.GetBiz(), request.GetBizId(), request.GetUid())
+	if err != nil {
+		return nil, err
+	}
+	return &intrv1.GetResponse{
+		Intr: i.toDTO(get),
+	}, nil
 }
 
 func (i *InteractiveServiceServer) GetByIds(ctx context.Context, request *intrv1.GetByIdsRequest) (*intrv1.GetByIdsResponse, error) {
+	//ids, err := i.svc.GetByIds(ctx, request.GetBiz(), request.GetIds())
+	//if err != nil {
+	//	return nil, err
+	//}
+	//res := &intrv1.GetByIdsResponse{
+	//	Intrs: make(map[string]intrv1.Interactive),
+	//}
+	//for i2, interactive := range ids {
+	//
+	//}
+	//return res, nil
 	//TODO implement me
 	panic("implement me")
 }
@@ -47,4 +73,20 @@ func (i *InteractiveServiceServer) GetByIds(ctx context.Context, request *intrv1
 func (i *InteractiveServiceServer) mustEmbedUnimplementedInteractiveServiceServer() {
 	//TODO implement me
 	panic("implement me")
+}
+
+// toDTO
+//
+//	@Description: Data transfer object
+func (i *InteractiveServiceServer) toDTO(item domain.Interactive) *intrv1.Interactive {
+	return &intrv1.Interactive{
+		Biz:        item.Biz,
+		BizId:      item.BizId,
+		ReadCnt:    item.ReadCnt,
+		LikeCnt:    item.LikeCnt,
+		CollectCnt: item.CollectCnt,
+		CommentCnt: item.CommentCnt,
+		Liked:      item.Liked,
+		Collected:  item.Collected,
+	}
 }
--- bbs-micro/bbs-interactive/service/interactive_service.go
+++ bbs-micro/bbs-interactive/service/interactive_service.go
@@ -7,11 +7,11 @@ import (
 
 //go:generate mockgen -source=./interactive_service.go -package=svcmocks -destination=./svcmocks/interactive.mock.go
 type InteractiveService interface {
-	IncrReadCnt(ctx context.Context, biz string, id int64) error
-	Like(ctx context.Context, biz string, id int64, id2 int64) error
-	CancelLike(ctx context.Context, biz string, id int64, id2 int64) error
+	IncrReadCnt(ctx context.Context, biz string, bizId int64) error
+	Like(ctx context.Context, biz string, bizId int64, uid int64) error
+	CancelLike(ctx context.Context, biz string, bizId int64, id2 int64) error
 	CollectArt(ctx context.Context, biz string, bizId int64, uId int64, cId int64) error
-	Get(ctx context.Context, biz string, id int64, uid int64) (domain.Interactive, error)
+	Get(ctx context.Context, biz string, bizId int64, uid int64) (domain.Interactive, error)
 	GetByIds(ctx context.Context, biz string, ids []int64) (map[int64]domain.Interactive, error)
 }
 
