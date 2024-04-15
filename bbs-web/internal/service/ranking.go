44bd30e1a65c1341f57d79d144556dcfaa757f77
wulinlin <18574945291@163.com>
2024-04-15 14:50:10 +0800

    这个测试我日了狗了，搞一天了 不搞了

--- bbs-web/internal/service/ranking.go
+++ bbs-web/internal/service/ranking.go
@@ -4,7 +4,6 @@ import (
 	"bbs-web/internal/domain"
 	"bbs-web/internal/service/article"
 	"bbs-web/pkg/utils/zifo/slice"
-	"fmt"
 	"math"
 
 	"context"
@@ -19,33 +18,33 @@ import (
 
 type RankingService interface {
 	TopN(ctx context.Context) error
-	topN(ctx context.Context) ([]domain.Article, error)
 	//TopN(ctx context.Context,n int64) error
 	//TopN(ctx context.Context, n int64) ([]any, error) // 后续写这个逻辑
 }
 
-type batchRankingService struct {
+type BatchRankingService struct {
 	artSvc    article.IArticleService
 	intrSvc   InteractiveService
 	batchSize int
 	queueCap  int
 
-	scoreFn func(t int64, likeCnt int64) float64 // 要求不能返回负数
+	scoreFn func(t time.Time, likeCnt int64) float64 // 要求不能返回负数
 }
 
-func NewBatchRankingService(artSvc article.IArticleService, intrSvc InteractiveService) *batchRankingService {
-	return &batchRankingService{
+func NewBatchRankingService(artSvc article.IArticleService, intrSvc InteractiveService) *BatchRankingService {
+	return &BatchRankingService{
 		artSvc:    artSvc,
 		intrSvc:   intrSvc,
 		batchSize: 100,
 		queueCap:  100,
-		scoreFn: func(t int64, likeCnt int64) float64 {
+		scoreFn: func(utime time.Time, likeCnt int64) float64 {
 			// Hacknews的公式
-			return float64(likeCnt-1) / math.Pow(float64(likeCnt+2), 1.5)
+			duration := time.Since(utime).Seconds()
+			return float64(likeCnt-1) / math.Pow(duration+2, 1.5)
 		},
 	}
 }
-func (svc *batchRankingService) TopN(ctx context.Context) error {
+func (svc *BatchRankingService) TopN(ctx context.Context) error {
 	n, err := svc.topN(ctx)
 	if err != nil {
 		return err
@@ -55,7 +54,7 @@ func (svc *batchRankingService) TopN(ctx context.Context) error {
 	return err
 }
 
-func (svc *batchRankingService) topN(ctx context.Context) ([]domain.Article, error) {
+func (svc *BatchRankingService) topN(ctx context.Context) ([]domain.Article, error) {
 	var (
 		offset = 0
 		start  = time.Now()
@@ -105,8 +104,7 @@ func (svc *batchRankingService) topN(ctx context.Context) ([]domain.Article, err
 				// 没有点赞数据
 				continue
 			}
-			score := svc.scoreFn(art.Utime.Unix(), intr.LikeCnt+2) // +2 为了规避负数问题
-			fmt.Println("当前的分数", art.Id, score)
+			score := svc.scoreFn(art.Utime, intr.LikeCnt) // +2 为了规避负数问题
 			node := Score{
 				art:   art,
 				score: score,
@@ -126,7 +124,7 @@ func (svc *batchRankingService) topN(ctx context.Context) ([]domain.Article, err
 
 		offset = offset + len(arts) // 否则就计算下一批
 	}
-	res := make([]domain.Article, 0, topQueue.Len())
+	res := make([]domain.Article, topQueue.Len())
 	for i := topQueue.Len() - 1; i >= 0; i-- {
 		ele, _ := topQueue.Dequeue()
 		res[i] = ele.art
--- bbs-web/internal/service/ranking_test.go
+++ bbs-web/internal/service/ranking_test.go
@@ -16,7 +16,7 @@ import (
 // @Date 2024-03-28 17:13
 
 func TestBatchRankingService_TopN(t *testing.T) {
-	nw := time.Now()
+	now := time.Now()
 	tests := []struct {
 		name string
 		mock func(ctrl *gomock.Controller) (article.IArticleService, InteractiveService)
@@ -29,30 +29,47 @@ func TestBatchRankingService_TopN(t *testing.T) {
 			mock: func(ctrl *gomock.Controller) (article.IArticleService, InteractiveService) {
 				artSvc := svcmocks.NewMockIArticleService(ctrl)
 				intrSvc := svcmocks.NewMockInteractiveService(ctrl)
-				artSvc.EXPECT().ListPub(gomock.Any(), nw, 0, 3).
+				// 先模拟批量获取数据
+				// 先模拟第一批
+				artSvc.EXPECT().ListPub(gomock.Any(), gomock.Any(), 0, 2).
 					Return([]domain.Article{
-						domain.Article{Id: 1, Utime: nw, Ctime: nw},
-						domain.Article{Id: 2, Utime: nw, Ctime: nw},
-						domain.Article{Id: 3, Utime: nw, Ctime: nw},
+						{Id: 1, Utime: now},
+						{Id: 2, Utime: now},
 					}, nil)
-				artSvc.EXPECT().ListPub(gomock.Any(), nw, 3, 3).
+				// 模拟第二批
+				artSvc.EXPECT().ListPub(gomock.Any(), gomock.Any(), 2, 2).
+					Return([]domain.Article{
+						{Id: 3, Utime: now},
+						{Id: 4, Utime: now},
+					}, nil)
+				// 模拟第三批
+				artSvc.EXPECT().ListPub(gomock.Any(), gomock.Any(), 4, 2).
+					// 没数据了
 					Return([]domain.Article{}, nil)
 
-				intrSvc.EXPECT().GetByIds(gomock.Any(), "article", []int64{
-					1, 2, 3,
-				}).Return(map[int64]domain.Interactive{
-					1: domain.Interactive{BizId: 1, LikeCnt: 1},
-					2: domain.Interactive{BizId: 2, LikeCnt: 2},
-					3: domain.Interactive{BizId: 3, LikeCnt: 3},
-				}, nil)
+				// 第一批的点赞数据
+				intrSvc.EXPECT().GetByIds(gomock.Any(), "article", []int64{1, 2}).
+					Return(map[int64]domain.Interactive{
+						1: {LikeCnt: 1},
+						2: {LikeCnt: 2},
+					}, nil)
+
+				// 第二批的点赞数据
+				intrSvc.EXPECT().GetByIds(gomock.Any(), "article", []int64{3, 4}).
+					Return(map[int64]domain.Interactive{
+						3: {LikeCnt: 3},
+						4: {LikeCnt: 4},
+					}, nil)
+
+				// 第三批的点赞数据
 				intrSvc.EXPECT().GetByIds(gomock.Any(), "article", []int64{}).
-					Return(map[int64]domain.Interactive{})
+					Return(map[int64]domain.Interactive{}, nil)
 				return artSvc, intrSvc
 			},
 			wantData: []domain.Article{
-				{Id: 3, Utime: nw, Ctime: nw},
-				{Id: 2, Utime: nw, Ctime: nw},
-				{Id: 1, Utime: nw, Ctime: nw},
+				{Id: 4, Utime: now},
+				{Id: 3, Utime: now},
+				{Id: 2, Utime: now},
 			},
 			wantErr: nil,
 		},
@@ -62,11 +79,14 @@ func TestBatchRankingService_TopN(t *testing.T) {
 			ctrl := gomock.NewController(t)
 			defer ctrl.Finish()
 			artSvc, intrSvc := tt.mock(ctrl)
-			svc := NewBatchRankingService(artSvc, intrSvc)
-			svc.batchSize = 3
-			svc.queueCap = 3
-			svc.scoreFn = func(t int64, likeCnt int64) float64 {
-				return float64(likeCnt)
+			svc := &BatchRankingService{
+				artSvc:    artSvc,
+				intrSvc:   intrSvc,
+				batchSize: 2,
+				queueCap:  3,
+				scoreFn: func(t time.Time, likeCnt int64) float64 {
+					return float64(likeCnt)
+				},
 			}
 			arts, err := svc.topN(context.Background())
 			assert.Equal(t, tt.wantErr, err)
