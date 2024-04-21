7ed1e3f9898f5fec8ca3d8c2fab3f23958861b50
高桥琳介 <18574945291@163.com>
2024-04-21 16:39:49 +0800

    新建Etcd演示注册中心 client注册

--- bbs-web/study/grpc/grpc_etcd_test.go
+++ bbs-web/study/grpc/grpc_etcd_test.go
@@ -7,16 +7,29 @@
 package grpc
 
 import (
+	"context"
+	"github.com/go-kit/kit/sd/etcdv3"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"testing"
+	"time"
 )
 
 type EtcdTestSuide struct {
 	suite.Suite
+	client etcdv3.Client
 }
 
-func (s *EtcdTestSuide) TestServer() {
+func (s *EtcdTestSuide) SetupSuite() {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	client, err := etcdv3.NewClient(ctx, []string{"localhost:12379"}, etcdv3.ClientOptions{})
+	require.NoError(s.T(), err)
+	s.client = client
 
+}
+func (s *EtcdTestSuide) TestServer() {
+	em, err := NewManager
 }
 
 func TestEtcd(t *testing.T) {
