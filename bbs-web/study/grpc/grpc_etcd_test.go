97f611316aa9b043401f9589c0a8e1ad2c3838f2
wulinlin <18574945291@163.com>
2024-04-22 17:16:36 +0800

    ectd续约

--- bbs-web/study/grpc/grpc_etcd_test.go
+++ bbs-web/study/grpc/grpc_etcd_test.go
@@ -9,6 +9,7 @@ package grpc
 import (
 	"bbs-web/study/grpc/hello"
 	"context"
+	"fmt"
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	clientv3 "go.etcd.io/etcd/client/v3"
@@ -69,6 +70,21 @@ func (s *EtcdTestSuide) TestClient() {
 }
 
 func (s *EtcdTestSuide) TestServer() {
+
+	l, err := net.Listen("tcp", "192.168.1.51:8090")
+	require.NoError(s.T(), err)
+
+	// 这个Context是控制创建租约的超时时间
+	ctx2, cancel2 := context.WithTimeout(context.Background(), time.Second)
+	defer cancel2()
+	// 创建租约
+	// ttl 租期 秒为单位
+	var ttl int64 = 30
+	leaseResp, err := s.client.Grant(ctx2, ttl)
+	if err != nil {
+		panic(err)
+	}
+
 	// endpoint 以服务为维度，一个服务一个endpoint
 	em, err := endpoints.NewManager(s.client, "service/user")
 	if err != nil {
@@ -81,38 +97,50 @@ func (s *EtcdTestSuide) TestServer() {
 	// key 就是指的实例 key ：1. 如果有instance id 就用instance id 2.用本地IP+Port
 	//key := fmt.Sprintf("")
 	key := "service/user/" + addr
+
+	// 在这一步之前 完成所有初始化工作 ==> 缓存预加载，配置预加载。。。。
 	err = em.AddEndpoint(ctx, key, endpoints.Endpoint{
 		Addr: addr,
-	})
+	}, clientv3.WithLease(leaseResp.ID))
 	if err != nil {
 		panic(err)
 	}
-	// 万一注册信息有变动的话 怎么办  比如注册端口改了？  开线程监听啦
+	// 开启goroutine 自动续约
+	kaCtx, kaCancel := context.WithCancel(context.Background())
 	go func() {
-		ticker := time.NewTicker(time.Second)
-		for now := range ticker.C {
-			ctx1, cancel1 := context.WithTimeout(context.Background(), time.Second)
-			defer cancel1()
-			// 覆盖的语义
-			em.AddEndpoint(ctx1, key, endpoints.Endpoint{
-				Addr:     addr,
-				Metadata: now.String(), // 元数据信息 分组信息，权重信息，机房信息，负载信息等等
-			})
-			//em.Update(ctx, []*endpoints.UpdateWithOpts{
-			//	{
-			//		Update: endpoints.Update{
-			//			Op:       endpoints.Delete,
-			//			Key:      key,
-			//			Endpoint: endpoints.Endpoint{Addr: addr},
-			//		},
-			//	},
-			//})
-			cancel1()
+		ch, err2 := s.client.KeepAlive(kaCtx, leaseResp.ID)
+		if err2 != nil {
+			panic(err2)
+		}
+		for kaResp := range ch {
+			fmt.Println("续约信息:", kaResp.String(), time.Now()) // 通常就是打印一下信息
 		}
 	}()
+	// 万一注册信息有变动的话 怎么办  比如注册端口改了？  开线程监听啦
+	//go func() {
+	//	ticker := time.NewTicker(time.Second)
+	//	for now := range ticker.C {
+	//		ctx1, cancel1 := context.WithTimeout(context.Background(), time.Second)
+	//		defer cancel1()
+	//		// 覆盖的语义
+	//		em.AddEndpoint(ctx1, key, endpoints.Endpoint{
+	//			Addr:     addr,
+	//			Metadata: now.String(), // 元数据信息 分组信息，权重信息，机房信息，负载信息等等
+	//		}, clientv3.WithLease(leaseResp.ID)) // 更新的时候 也要带上租约信息
+	//		// 注意 不能在一个UpdateWithOpts中，对一个key只能进行一次操作，不然会报错  存疑
+	//		//em.Update(ctx, []*endpoints.UpdateWithOpts{
+	//		//	{
+	//		//		Update: endpoints.Update{
+	//		//			Op:       endpoints.Delete,
+	//		//			Key:      key,
+	//		//			Endpoint: endpoints.Endpoint{Addr: addr},
+	//		//		},
+	//		//	},
+	//		//})
+	//		cancel1()
+	//	}
+	//}()
 
-	l, err := net.Listen("tcp", "192.168.1.51:8090")
-	require.NoError(s.T(), err)
 	server := grpc.NewServer()
 	hello.RegisterHelloServiceServer(server, &Svc{})
 
@@ -123,7 +151,13 @@ func (s *EtcdTestSuide) TestServer() {
 	// 退出的时候 记得Delete
 	ctx, cancel = context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
-	err = em.DeleteEndpoint(ctx, key)
+
+	/**
+	删除的步骤  1.取消续约  2. 将自己从注册中心删除 2.停止服务
+	*/
+	kaCancel()                        // 取消续约
+	err = em.DeleteEndpoint(ctx, key) // 取消注册
+	server.GracefulStop()             // 优雅退出
 }
 
 func TestEtcd(t *testing.T) {
