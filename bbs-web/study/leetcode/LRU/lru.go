f10029a4e536bd5068c3c9cd02af65eaf8064a64
wulinlin <18574945291@163.com>
2024-05-07 10:09:23 +0800

    盲写LRU缓存

--- bbs-web/study/leetcode/LRU/lru.go
+++ bbs-web/study/leetcode/LRU/lru.go
@@ -6,25 +6,71 @@
 
 package LRU
 
-import "container/list"
-
+// 双端链表的节点
 type node struct {
+	prev, next *node
+	data       int
 }
 
 type LRUCache struct {
-	maxSize  int        // 最大容量
-	linkList *list.List // 双向链表
-
+	maxSize int // 最大容量
+	head    *node
+	tail    *node
+	hashMap map[int]*node // 哈希表
 }
 
 func Constructor(capacity int) LRUCache {
-
+	return LRUCache{
+		maxSize: capacity,
+		head: &node{
+			prev: nil,
+			next: nil,
+			data: 0,
+		},
+		tail:    nil,
+		hashMap: make(map[int]*node, 0),
+	}
 }
 
 func (this *LRUCache) Get(key int) int {
-
+	// 从map中获取元素
+	val, ok := this.hashMap[key]
+	if !ok {
+		return -1
+	}
+	// 说明有值，要将这个节点移动到栈顶
+	this.moveToNo1(val)
+	return val.data
 }
 
 func (this *LRUCache) Put(key int, value int) {
-
+	if len(this.hashMap) >= this.maxSize {
+		this.removeLast()
+	}
+	n := &node{
+		data: value,
+		next: this.head.next,
+		prev: this.head,
+	}
+	this.hashMap[key] = n
+	this.head.next.prev = n
+	this.head.next = n
+}
+func (this *LRUCache) removeLast() {
+	// 1. 移除链表的最后一个元素
+	// 2. 删除最后一个元素在map中的记录
+}
+func (this *LRUCache) moveToNo1(cur *node) {
+	if cur == nil {
+		return
+	}
+	if cur.prev == this.head {
+		// 说明已经是第一个了，不用管了
+		return
+	}
+	cur.prev.next = cur.next
+	cur.next.prev = cur.prev
+	cur.next = this.head.next
+	cur.prev = this.head
+	this.head.next = cur
 }
